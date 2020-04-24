65086371b5352c014eae3246e5ddd79859f0d0f3
Xenia Plotnikova <lisaplotnikova@yandex.ru>
2020-04-24 16:01:02 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
